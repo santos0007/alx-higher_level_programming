5b6a6b84da38f26c7621f82ea6ae8f34a832cf06
santos0007 <shingsantonio@gmail.com>
2023-08-30 16:38:54 +0200

    data structures python project

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
